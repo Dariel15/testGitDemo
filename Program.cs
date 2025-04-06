1b80df8da93760fb43cef12912169ae7857a16f9
Dariel15 <92190185+Dariel15@users.noreply.github.com>
2025-04-07 07:52:13 +0800

    Keeping one message

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
-Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
-Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
-Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
-Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
-Console.WriteLine("Hello, C# Mabuhay! Nagbago ka na ><");
